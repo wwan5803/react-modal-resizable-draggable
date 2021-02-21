a6c01d047768a066f29af7ff6f75ff1e7880eb70
Hlöðver Sigurðsson <hlolli@gmail.com>
2021-02-21 13:22:18 +0100

    upgrade react, babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,130 +1,98 @@
 const webpack = require("webpack");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-var path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+var path = require("path");
 
 var devOption = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].js',
-        libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                exclude: /(node_modules|bower_components|build)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [ 'css-loader' ]
-            }
-        ]
-    },
-    externals: {
-        'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                comparisons: false
-            },
-            output: {
-                comments: false,
-                ascii_only: true
-            },
-            sourceMap: true
-        })
-    ]
-
-}
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+    libraryTarget: "commonjs2", // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["css-loader"],
+      },
+    ],
+  },
+  externals: {
+    react: "commonjs react", // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  },
+  plugins: [],
+};
 
 var reactExternal = {
-    root: 'React',
-    commonjs2: 'react',
-    commonjs: 'react',
-    amd: 'react'
+  root: "React",
+  commonjs2: "react",
+  commonjs: "react",
+  amd: "react",
 };
 var reactDOMExternal = {
-    root: 'ReactDOM',
-    commonjs2: 'react-dom',
-    commonjs: 'react-dom',
-    amd: 'react-dom'
+  root: "ReactDOM",
+  commonjs2: "react-dom",
+  commonjs: "react-dom",
+  amd: "react-dom",
 };
 
 var productOption = {
-    entry: './src/index.js',
-    output: {
-        filename: '[name].js',
-        chunkFilename: '[id].chunk.js',
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/',
-        libraryTarget: 'umd',
-        library: 'FlexibleReactModal'
-    },
+  entry: "./src/index.js",
+  mode: "production",
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[id].chunk.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    libraryTarget: "umd",
+    library: "FlexibleReactModal",
+  },
 
-    // output: {
-    //     path: path.resolve(__dirname, 'build'),
-    //     chunkFilename: '[name].[chunkhash:8].js',
-    //     filename: '[name].[chunkhash:8].js',
-    //     libraryTarget: 'commonjs2'
-    // },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                exclude: /(node_modules|bower_components|build)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [ 'css-loader' ]
-            }
-        ]
-    },
-    externals: {
-        'react': reactExternal,
-        'react-dom': reactDOMExternal
-    },
-    // externals: {
-    //     'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-    // },
-    plugins: [
-        new CleanWebpackPlugin([path.resolve(__dirname, 'build')]),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-                comparisons: false,
-                drop_console: true
-            },
-            output: {
-                comments: false,
-                ascii_only: true
-            }
-        })
-    ]
+  // output: {
+  //     path: path.resolve(__dirname, 'build'),
+  //     chunkFilename: '[name].[chunkhash:8].js',
+  //     filename: '[name].[chunkhash:8].js',
+  //     libraryTarget: 'commonjs2'
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["css-loader"],
+      },
+    ],
+  },
 
-}
-
+  externals: {
+    react: reactExternal,
+    "react-dom": reactDOMExternal,
+  },
+  // externals: {
+  //     'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  // },
+  plugins: [new CleanWebpackPlugin()],
+};
 
 module.exports = (env) => {
-    console.log('env', env)
-    if(env==='production'){
-        return productOption
-    }else{
-        return devOption
-    }
+  console.log("env", env);
+  if (env === "production") {
+    return productOption;
+  } else {
+    return devOption;
+  }
 };
